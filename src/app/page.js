5a101b03145a9e41ba626f384f5f10c3b4c65748
jubairahmed22 <jubairahmed060@gmail.com>
2024-03-24 13:38:23 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Counter from "@/components/Counter/Counter";
+import variables from '../styles/variables.module.scss'
 
 const HomePage = () => {
 
 
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-2xl font-bold">Welcome time to Next.js home page </h1>
+    <div style={{ background: variables.secondaryColor }}>
+      <div style={{ color: variables.primaryColor }}>SCSS</div>
+      <h1 >Welcome time to Next.js home page </h1>
       <Counter></Counter>
     </div>
   );
